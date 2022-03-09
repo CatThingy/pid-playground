29f5e94206a51311757c9793de1935e4efa1e944
CatThingy <andyc.andychen@gmail.com>
2022-03-09 15:21:44 -0800

    Update UI

--- src/app.rs
+++ src/app.rs
@@ -31,146 +31,33 @@ impl epi::App for Application {
     }
 
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
-        ctx.set_visuals(egui::Visuals::dark());
         let mut all_dirty = false;
 
-        egui::SidePanel::right("Settings")
-            .resizable(false)
-            .show(ctx, |ui| {
-                ui.heading("Tuning");
-                ui.separator();
+        if let Some(_) = frame.info().prefer_dark_mode {
+            ctx.set_visuals(egui::Visuals::dark());
+        }
 
-                let mut duplicated_models = Vec::<Model>::new();
-                let mut deleted_models = Vec::<u64>::new();
+        let s = ctx.input().screen_rect();
 
-                egui::ScrollArea::vertical()
-                    .max_height(300.0)
-                    .show(ui, |ui| {
-                        for model in self.models.iter_mut() {
-                            tuning_ui(model, ui);
-                            if ui.button("Duplicate").clicked() {
-                                let mut new_model = model.clone();
-                                new_model.dirty = true;
-                                new_model.id = self.last_model_id;
-                                self.last_model_id += 1;
-                                self.values.insert(new_model.id, vec![]);
-                                duplicated_models.push(new_model);
-                            }
-
-                            if ui.button("Delete").clicked() {
-                                deleted_models.push(model.id);
-                            }
-                            ui.separator();
-                        }
-                        if ui.button("Add new model").clicked() {
-                            let mut model = Model::new(
-                                &("Model ".to_owned() + &self.last_model_id.to_string()),
-                                self.last_model_id,
-                            );
-
-                            model.dirty = true;
-                            self.last_model_id += 1;
-                            self.values.insert(model.id, vec![]);
-                            self.models.push(model);
-                        }
-                    });
-
-                for model in duplicated_models {
-                    self.models.push(model);
-                }
-
-                for id in deleted_models {
-                    self.models = self
-                        .models
-                        .to_vec()
-                        .into_iter()
-                        .filter(|v| v.id != id)
-                        .collect::<Vec<Model>>();
-                }
-
-                ui.separator();
-
-                egui::Grid::new("Simulation")
-                    .num_columns(2)
-                    .striped(true)
-                    .max_col_width(100.0)
-                    .min_col_width(100.0)
-                    .show(ui, |ui| {
-                        ui.heading("Simulation");
-                        ui.end_row();
-
-                        ui.label("Damping");
-                        let damp_res = ui.add_sized(
-                            ui.available_size(),
-                            egui::widgets::DragValue::new(&mut self.env.damping)
-                                .speed(0.05)
-                                .clamp_range(0.0..=100.0),
-                        );
-                        ui.end_row();
-                        ui.label("Setpoint");
-                        let setpoint_res = ui.add_sized(
-                            ui.available_size(),
-                            egui::widgets::DragValue::new(&mut self.env.setpoint)
-                                .speed(1)
-                                .clamp_range(0.0..=150.0),
-                        );
-                        ui.end_row();
-
-                        ui.label("Applied force");
-                        let force_res = ui.add_sized(
-                            ui.available_size(),
-                            egui::widgets::DragValue::new(&mut self.env.applied_force)
-                                .speed(0.05)
-                                .clamp_range(-10.0..=10.0),
-                        );
-                        ui.end_row();
-
-                        ui.label("Time step");
-                        let timestep_res = ui.add_sized(
-                            ui.available_size(),
-                            egui::widgets::DragValue::new(&mut self.env.timestep)
-                                .speed(0.005)
-                                .clamp_range(0.001..=1.0),
-                        );
-                        ui.end_row();
-
-                        ui.end_row();
-
-                        ui.label("Realtime sim.");
-                        let checkbox_res = ui.checkbox(&mut self.realtime, "");
-
-                        if !all_dirty
-                            && (damp_res.changed()
-                                || force_res.changed()
-                                || setpoint_res.changed()
-                                || timestep_res.changed()
-                                || (checkbox_res.changed() && !self.realtime))
-                        {
-                            all_dirty = true;
-                        }
-                    });
-                if ui
-                    .add_enabled(
-                        self.realtime,
-                        egui::widgets::Button::new("Reset simulation"),
-                    )
-                    .clicked()
-                {
-                    for model in self.models.iter_mut() {
-                        model.reset();
-                        if let Some(v) = self.values.get_mut(&model.id) {
-                            v.clear();
-                        };
-                    }
-                };
-
-                egui::TopBottomPanel::bottom("TEST")
-                    .frame(egui::Frame::none())
-                    .show_inside(ui, |ui| {
-                        ui.separator();
-                        ui.label("Hello!");
-                    })
-            });
+        if s.width() > s.height() {
+            egui::SidePanel::right("Settings")
+                .max_width(215.0)
+                .resizable(false)
+                .show(ctx, |ui| {
+                    self.info_panel(ui, &mut all_dirty, true);
+                });
+        } else {
+            egui::TopBottomPanel::bottom("Settings")
+                .max_height(200.0)
+                .resizable(false)
+                .show(ctx, |ui| {
+                    egui::ScrollArea::vertical()
+                        .max_height(400.0)
+                        .show(ui, |ui| {
+                            self.info_panel(ui, &mut all_dirty, false);
+                        });
+                });
+        }
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("PID Playground");
@@ -285,6 +172,155 @@ impl epi::App for Application {
                 model.dirty = false;
             }
         }
+    }
+}
+
+impl Application {
+    fn info_panel(&mut self, ui: &mut Ui, all_dirty: &mut bool, is_vertical: bool) {
+        ui.heading("Tuning");
+        ui.separator();
+
+        let mut duplicated_models = Vec::<Model>::new();
+        let mut deleted_models = Vec::<u64>::new();
+
+        let scroll = match is_vertical {
+            true => egui::ScrollArea::vertical().max_height(300.0),
+            false => egui::ScrollArea::horizontal().max_height(200.0),
+        };
+
+        let mut scroll_item = |ui: &mut Ui, app: &mut Application| {
+            for model in app.models.iter_mut() {
+                ui.separator();
+                ui.vertical(|ui| {
+                    tuning_ui(model, ui);
+                    if ui.button("Duplicate").clicked() {
+                        let mut new_model = model.clone();
+                        new_model.dirty = true;
+                        new_model.id = app.last_model_id;
+                        app.last_model_id += 1;
+                        app.values.insert(new_model.id, vec![]);
+                        duplicated_models.push(new_model);
+                    }
+
+                    if ui.button("Delete").clicked() {
+                        deleted_models.push(model.id);
+                    }
+                });
+            }
+        };
+
+        scroll.show(ui, |ui| match is_vertical {
+            true => {
+                scroll_item(ui, self);
+            }
+            false => {
+                ui.horizontal(|ui| scroll_item(ui, self));
+            }
+        });
+
+        for model in duplicated_models {
+            self.models.push(model);
+        }
+
+        for id in deleted_models {
+            self.models = self
+                .models
+                .to_vec()
+                .into_iter()
+                .filter(|v| v.id != id)
+                .collect::<Vec<Model>>();
+        }
+
+        ui.separator();
+        if ui.button("Add new model").clicked() {
+            let mut model = Model::new(
+                &("Model ".to_owned() + &self.last_model_id.to_string()),
+                self.last_model_id,
+            );
+
+            model.dirty = true;
+            self.last_model_id += 1;
+            self.values.insert(model.id, vec![]);
+            self.models.push(model);
+        }
+
+        ui.separator();
+
+        egui::Grid::new("Simulation")
+            .num_columns(2)
+            .striped(true)
+            .max_col_width(100.0)
+            .min_col_width(100.0)
+            .show(ui, |ui| {
+                ui.heading("Simulation");
+                ui.end_row();
+
+                let size = Vec2::new(80.0, ui.available_height());
+
+                ui.label("Damping");
+                let damp_res = ui.add_sized(
+                    size,
+                    egui::widgets::DragValue::new(&mut self.env.damping)
+                        .speed(0.05)
+                        .clamp_range(0.0..=100.0),
+                );
+                ui.end_row();
+                ui.label("Setpoint");
+                let setpoint_res = ui.add_sized(
+                    size,
+                    egui::widgets::DragValue::new(&mut self.env.setpoint)
+                        .speed(1)
+                        .clamp_range(0.0..=150.0),
+                );
+                ui.end_row();
+
+                ui.label("Applied force");
+                let force_res = ui.add_sized(
+                    size,
+                    egui::widgets::DragValue::new(&mut self.env.applied_force)
+                        .speed(0.05)
+                        .clamp_range(-10.0..=10.0),
+                );
+                ui.end_row();
+
+                ui.label("Time step");
+                let timestep_res = ui.add_sized(
+                    size,
+                    egui::widgets::DragValue::new(&mut self.env.timestep)
+                        .speed(0.005)
+                        .clamp_range(0.001..=1.0),
+                );
+                ui.end_row();
+
+                ui.end_row();
+
+                ui.label("Realtime sim.");
+                let checkbox_res = ui.checkbox(&mut self.realtime, "");
+
+                if !*all_dirty
+                    && (damp_res.changed()
+                        || force_res.changed()
+                        || setpoint_res.changed()
+                        || timestep_res.changed()
+                        || (checkbox_res.changed() && !self.realtime))
+                {
+                    *all_dirty = true;
+                }
+            });
+        if ui
+            .add_enabled(
+                self.realtime,
+                egui::widgets::Button::new("Reset simulation"),
+            )
+            .clicked()
+        {
+            for model in self.models.iter_mut() {
+                model.reset();
+                if let Some(v) = self.values.get_mut(&model.id) {
+                    v.clear();
+                };
+            }
+        };
     }
 }
 
